6c03d134b15c04baf5f7724d0b0e06c0d31eaa40
Gisela Keidel <giselakeidel@gmail.com>
2020-05-29 12:03:02 -0300

    rails new

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,21 +1,30 @@
 class CocktailsController < ApplicationController
-
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
   end
 
-  def edit
+  def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
-  def update
-  end
+  private
 
-  def destroy
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
-
 end
